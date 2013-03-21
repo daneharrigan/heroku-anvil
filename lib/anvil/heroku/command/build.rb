968f298869a633fbf6b380fa06afd93ec516eeaf
Dane Harrigan <dane.harrigan@gmail.com>
2013-03-21 14:38:44 -0700

    rename --release [APP] to --release APP

--- lib/anvil/heroku/command/build.rb
+++ lib/anvil/heroku/command/build.rb
@@ -24,7 +24,7 @@ class Heroku::Command::Build < Heroku::Command::Base
   #
   # -b, --buildpack URL  # use a custom buildpack
   # -p, --pipeline       # pipe compile output to stderr and only put the slug url on stdout
-  # -r, --release [APP]  # release the slug to an app (defaults to current app detected from git)
+  # -r, --release APP    # release the slug to an app (defaults to current app detected from git)
   # -t, --type TYPE      # build a slug of the given type (tgz, deb)
   #
   def index
